8d9ef8b9ebc71079f229d040ac5464f3199123d0
LewisContreras <legiconba4@gmail.com>
2021-04-30 22:47:13 -0500

    Hago la base de datos de mascotas

---
+++ js/dataBase.js
@@ -0,0 +1,135 @@
+export const dataDogs = {
+    "p001" : {
+        "namePet" : "Rocky",
+        "breed" : "Rottweiller",
+        "age" : "3 Meses",
+        "sex" : "Male",
+        "location" : "4140 Parker Rd. Allentown, New Mexico 31134",
+        "personality" : ["Cariñoso", "Inquieto", "Juguetón"],
+        "History" : "Rocky es un perrito muy lindo y cariñoso, tiene 5 hermanitos más y por cuestiones de espacio y tiempo no podremos cuidar a todos, nuestra misión es encontrar la familia ideal para él y seguro que tú eres la persona indicada.",
+        "petPhoto" : "img/rocky.png",
+        "owner" : "Maria Dolores",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "p001"
+    },
+    "p002" : {
+        "namePet" : "Bruno",
+        "breed" : "Boxer",
+        "age" : "2 Meses",
+        "sex" : "Male",
+        "location" : "Al frente de Rocky",
+        "personality" : ["Inquieto", "Cariñoso", "Tierno"],
+        "History" : "Bruno es un perrito muy lindo y cariñoso, tiene 5 hermanitos más y por cuestiones de espacio y tiempo no podremos cuidar a todos, nuestra misión es encontrar la familia ideal para él y seguro que tú eres la persona indicada.",
+        "petPhoto" : "img/bruno.png",
+        "owner" : "Oscar Zápata",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "p002"
+    },
+    "p003" : {
+        "namePet" : "Chester",
+        "breed" : "Golden Retriever",
+        "age" : "4 Meses",
+        "sex" : "Male",
+        "location" : "Diagonal a Bruno",
+        "personality" : ["Tierno", "Juguetón", "Cariñoso"],
+        "History" : "Chester es un perrito muy lindo y cariñoso, tiene 5 hermanitos más y por cuestiones de espacio y tiempo no podremos cuidar a todos, nuestra misión es encontrar la familia ideal para él y seguro que tú eres la persona indicada.",
+        "petPhoto" : "img/chester.png",
+        "owner" : "Luis Suarez",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "p003"
+    },
+    "p004" : {
+        "namePet" : "Papi",
+        "breed" : "Chihuahua",
+        "age" : "3 Meses",
+        "sex" : "Male",
+        "location" : "A la vuelta de Chester",
+        "personality" : ["Cariñoso", "Tierno", "Juguetón"],
+        "History" : "Papi es un perrito muy lindo y cariñoso, tiene 5 hermanitos más y por cuestiones de espacio y tiempo no podremos cuidar a todos, nuestra misión es encontrar la familia ideal para él y seguro que tú eres la persona indicada.",
+        "petPhoto" : "img/papi.png",
+        "owner" : "Bruno Díaz",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "p004"
+    },
+    "p005" : {
+        "namePet" : "Pelusa",
+        "breed" : "Bichón Frisé",
+        "age" : "5 Meses",
+        "sex" : "Female",
+        "location" : "En la mansión Foster",
+        "personality" : ["Inquieto", "Cariñoso", "Tierno"],
+        "History" : "Pelusa es un perrito muy lindo y cariñoso, tiene 5 hermanitos más y por cuestiones de espacio y tiempo no podremos cuidar a todos, nuestra misión es encontrar la familia ideal para él y seguro que tú eres la persona indicada.",
+        "petPhoto" : "img/pelusa.png",
+        "owner" : "Marco Reus",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "p005"
+    }
+}
+
+export const dataCats = {
+    "g001" : {
+        "namePet" : "Matilde",
+        "breed" : "British Shorthair",
+        "age" : "6 Meses",
+        "sex" : "Female",
+        "location" : "4140 Parker Rd. Allentown, New Mexico 31134",
+        "personality" : ["Juguetón", "Inquieto", "Tierno"],
+        "History" : "Matilde es la mejor guarda espaldas, siempre esta hay para conversar y recibirá cada día con un fuerte cariño. Fue rescatada y busco una persona que le pueda dar todo el amor que ella necesita.",
+        "petPhoto" : "img/matilde.png",
+        "owner" : "Juan Jesus",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "g001"
+    },
+    "g002" : {
+        "namePet" : "Kity",
+        "breed" : "Angora",
+        "age" : "5 Meses",
+        "sex" : "Male",
+        "location" : "4140 Parker Rd. Allentown, New Mexico 31134",
+        "personality" : ["Cariñoso", "Inquieto", "Tierno"],
+        "History" : "Kity es la mejor guarda espaldas, siempre esta hay para conversar y recibirá cada día con un fuerte cariño. Fue rescatada y busco una persona que le pueda dar todo el amor que ella necesita.",
+        "petPhoto" : "img/kity.png",
+        "owner" : "Elver Pardo",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "g002"
+    },
+    "g003" : {
+        "namePet" : "Ra",
+        "breed" : "Esfinge",
+        "age" : "4 Meses",
+        "sex" : "Male",
+        "location" : "4140 Parker Rd. Allentown, New Mexico 31134",
+        "personality" : ["Juguetón", "Inquieto", "Tierno"],
+        "History" : "Ra es la mejor guarda espaldas, siempre esta hay para conversar y recibirá cada día con un fuerte cariño. Fue rescatada y busco una persona que le pueda dar todo el amor que ella necesita.",
+        "petPhoto" : "img/ra.png",
+        "owner" : "Cesar Días",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "g003"
+    },
+    "g004" : {
+        "namePet" : "Bombón",
+        "breed" : "Bombay",
+        "age" : "3 Meses",
+        "sex" : "Female",
+        "location" : "4140 Parker Rd. Allentown, New Mexico 31134",
+        "personality" : ["Juguetón", "Inquieto", "Tierno"],
+        "History" : "Bombón es la mejor guarda espaldas, siempre esta hay para conversar y recibirá cada día con un fuerte cariño. Fue rescatada y busco una persona que le pueda dar todo el amor que ella necesita.",
+        "petPhoto" : "img/bombon.png",
+        "owner" : "Alba Peréz",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "g004"
+    },
+    "g005" : {
+        "namePet" : "Siami",
+        "breed" : "Siamés",
+        "age" : "7 Meses",
+        "sex" : "Female",
+        "location" : "4140 Parker Rd. Allentown, New Mexico 31134",
+        "personality" : ["Juguetón", "Inquieto", "Tierno"],
+        "History" : "Siami es la mejor guarda espaldas, siempre esta hay para conversar y recibirá cada día con un fuerte cariño. Fue rescatada y busco una persona que le pueda dar todo el amor que ella necesita.",
+        "petPhoto" : "img/siami.png",
+        "owner" : "Pepita Lupe",
+        "ownerPhoto" : "img/elmejor.jpg",
+        "id": "g005"
+    }
+}
