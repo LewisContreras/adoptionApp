32334de4e7758ff0deaba5341ca9d6276000d039
LewisContreras <legiconba4@gmail.com>
2021-05-01 19:18:53 -0500

    Termino la parte funcional del perfil

---
+++ js/user.js
@@ -0,0 +1,45 @@
+let nameInput = document.getElementById("name-input");
+let lastnameInput = document.getElementById("lastname-input");
+let emailInput = document.getElementById("email-input");
+let nameCurrent = document.getElementById("name-current-info");
+let userForm = document.querySelector(".user-form");
+let editAccount = document.getElementById("edit-account");
+
+if (localStorage.getItem("userInfo")) {
+    let userInfoOb = JSON.parse(localStorage.getItem("userInfo"));
+    nameCurrent.textContent = userInfoOb.nameUser + " " + userInfoOb.lastname;
+    nameInput.value = userInfoOb.nameUser;
+    lastnameInput.value = userInfoOb.lastname;
+    emailInput.value = userInfoOb.email;
+}
+
+editAccount.addEventListener("click", ()=>userForm.reset())
+
+
+
+
+
+
+
+
+
+
+
+let userInfo ={}
+
+
+
+
+
+userForm.addEventListener("submit", submitForm);
+
+
+function submitForm(e) {
+    e.preventDefault();
+    userInfo.nameUser = nameInput.value;
+    userInfo.lastname = lastnameInput.value;
+    userInfo.email = emailInput.value;
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    location.reload();
+}
+
